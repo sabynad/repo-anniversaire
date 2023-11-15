34ec587db394758cd544d2f0e2960882bd9aefc1
sabri chafroud <chafroud.sabri@hotmail.fr>
2023-11-15 14:41:07 +0100

    15/11/23 14h40

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,44 @@
 console.log("test code");
+     
+let dateActuelle = new Date();
 
 
-/**Le code attache un écouteur d'événement à l'élément avec la classe "btn-envoyer". 
-Cela signifie que lorsque vous cliquez sur le bouton,
- le code contenu dans la fonction sera exécuté*/
 document.querySelector('.btn-envoyer').addEventListener('click', function() {
-
-    /**Le code récupère les valeurs des éléments "jour" et "mois" à l'aide de la querySelectorméthode.
-     Ces éléments sont supposés être des champs de saisie dans lesquels 
-     l'utilisateur saisit son jour et son mois de naissance.*/ 
     let jour = document.querySelector('#jour').value;
     let mois = document.querySelector('#mois').value;
-
-   
-
-
-    /**Le code crée un nouvel Dateobjet pour la date actuelle en utilisant new Date(). 
-    Il crée également 
-    un nouvel Dateobjet pour l'anniversaire spécifié en utilisant 
-    les valeurs moiset jour.*/
-    // Calculer le nombre de jours restants jusqu'à l'anniversaire
-    let dateActuelle = new Date();
     let dateAnniversaire = new Date(dateActuelle.getFullYear(), mois - 1, jour);
+
+    // si le jour ou le mois n'est pas renseigner alors on affiche rien
+    if (jour == "" || mois == "" ){
+        return resultat.textContent = ("Veuillez saisir les champs");
+       
+    }
+    if (jour == dateActuelle.getDate() && mois == dateActuelle.getMonth()+1){
+        resultat.textContent = ("Joyeux Anniversaire !")
+        console.log("Joyeux Anniversaire!");
+    }
     
+    else { 
+
+
     if (dateAnniversaire < dateActuelle) {
         dateAnniversaire.setFullYear(dateActuelle.getFullYear() + 1);
     }
-    
+
     let differenceEnJours = Math.ceil((dateAnniversaire - dateActuelle) / (1000 * 60 * 60 * 24));
     
     // Afficher le résultat
     let resultat = document.querySelector("#resultat");
     resultat.textContent = ("Il reste " + differenceEnJours + " jours jusqu'à ton anniversaire !");
 
-    document.querySelector(".btn-reset").addEventListener('click', function() {
+    //supprimer les champs a l'appuie du bouton reset
+     document.querySelector(".btn-reset").addEventListener('click', function() {
      document.querySelector("#jour").value = "";
      document.querySelector("#mois").value = "";
      document.querySelector("#resultat").textContent = "";
-  
-    })
-    
+      
+    })  
+    }
 })
 
 
@@ -47,3 +46,7 @@ document.querySelector('.btn-envoyer').addEventListener('click', function() {
 
 /**Si l'anniversaire spécifié est déjà passé dans l'année en cours, le code définit 
 l'année de l'anniversaire sur l'année suivante à l'aide de setFullYear()*/
+
+// La méthode getFullYear() renvoie l'année de la date renseignée d'après l'heure locale.
+
+// La méthode setFullYear() définit l'année complête pour une date, d'après l'heure locale.
